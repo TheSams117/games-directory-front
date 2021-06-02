e0834ed2f54a3f5c980944c68a6564a649861ad5
Johan Camilo Cortes Ocampo <camilo4182@gmail.com>
2021-06-02 12:45:34 -0500

    .env.production file added and API variable renamed to REACT_APP_API

--- src/controllers/GamesController.js
+++ src/controllers/GamesController.js
@@ -4,7 +4,7 @@ export const GamesController = {
     add(game) {
         return axios({
             method: 'POST',
-            baseURL: process.env.API,
+            baseURL: process.env.REACT_APP_API,
             url: 'games',
             data: game
         });
@@ -12,21 +12,21 @@ export const GamesController = {
     list() {
         return axios({
             method: 'GET',
-            baseURL: process.env.API,
+            baseURL: process.env.REACT_APP_API,
             url: 'games'
         });
     },
     delete(idGame) {
         return axios({
             method: 'DELETE',
-            baseURL: process.env.API,
+            baseURL: process.env.REACT_APP_API,
             url: `games/${idGame}`
         });
     },
     update(game) {
         return axios({
             method: 'PUT',
-            baseURL: process.env.API,
+            baseURL: process.env.REACT_APP_API,
             url: `games`,
             data: game
         });
